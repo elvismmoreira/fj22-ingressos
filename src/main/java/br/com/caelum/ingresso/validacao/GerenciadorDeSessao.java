d2ba5d35d317cd5a21289e9b2ff252d732f033d9
Elvis Moreira <elvismmoreira@gmail.com>
2019-01-10 22:38:34 -0200

    adicionando preco

--- src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
+++ src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
@@ -1,5 +1,42 @@
 package br.com.caelum.ingresso.validacao;
 
-public class GerenciadorDeSessao {
+import java.time.LocalDate;
+import java.util.List;
 
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadorDeSessao {
+	
+	private List<Sessao> sessoesDaSala;
+	
+	public GerenciadorDeSessao(List<Sessao> sessoesDaSala) {
+		this.sessoesDaSala = sessoesDaSala;
+	}
+
+	private boolean horarioIsConflitante(Sessao sessaoExixtente, Sessao sessaoNova) {
+		
+		LocalDate hoje = LocalDate.now();
+		
+		LocalDateTime horarioSessaoExistente = sessaoExistente.getHorario().atDate(hoje);
+		LocalDateTime horarioSessaoNova = sessaoNova.getHorario().atDate(hoje);
+		
+		boolean terminaAntes = sessaoNova.getHorarioTermino()
+				              .isBefore(horarioExistente);
+		boolean comecaDepois = sessaoExistente.getHorarioTermino()
+				              .isBefore(horarioNova);
+		
+		if (terminaAntes || comecaDepois) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean cabe(Sessao sessaoNova) {
+		
+		return sessoesDaSala.parallelStream().noneMatch(sessaoExixtente ->
+		                                      horarioIsConflitante(sessaoExistente, sessaoNova));
+	}
+	
+	
+	
 }
--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -1,8 +1,20 @@
 package br.com.caelum.ingresso.validacao;
 
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
-@BEFORE:
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+
 public class GerenciadorDeSessaoTest {
 
 	private Filme rogueOne;
@@ -14,18 +26,20 @@ public class GerenciadorDeSessaoTest {
 	@Before
 	public void preparaSessoes() {
 		
-		this.rogueOne = new Filme("Rogue one", Duration.ofMinutes(120), "SCI-FI");
-		this.sala3D = new Sala("Sala 3D");
+		this.rogueOne = new Filme("Rogue one", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
+		this.sala3D = new Sala("Sala 3D", BigDecimal.TEN);
 		
 		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
 		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), rogueOne, sala3D);
-		this.sessaoDasDezoioto = new Sessao(LocalTime.parse("18:00:00"), rogueOne, sala3D);
+		this.sessaoDasDezoito = new Sessao(LocalTime.parse("18:00:00"), rogueOne, sala3D);
 		
 	}
 	
 	@Test
 	public void garanteQueNaoDevePermitirSessaoNoMesmoHorario() {
 		
-		List<Sessao>
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		Assert.assertFalse(gerenciador.cabe(sessaoDasDez));
 	}
 }
