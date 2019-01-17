a4a7d081ac7142052fe318fb81d9bdbfbf2ffac5
Elvis Moreira <elvismmoreira@gmail.com>
2019-01-17 19:13:07 -0200

    finalizando compra

--- src/main/java/br/com/caelum/ingresso/model/ImagemCapa.java
+++ src/main/java/br/com/caelum/ingresso/model/ImagemCapa.java
@@ -1,5 +1,17 @@
 package br.com.caelum.ingresso.model;
 
-public class ImagemCapa {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class ImagemCapa {
+	
+	@JsonProperty("Poster")
+	String url;
+	
+	public String getUrl() {
+		return url;
+	}
+   
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }
